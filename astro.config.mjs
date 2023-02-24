350d37d76809abc7fe15b3c2b766e77bba2357ac
Jose Eduardo Monteiro <josee.edumont@gmail.com>
2023-02-24 09:33:53 -0300

    fix shiki+vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,22 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
+
+
+
+// https://github.com/withastro/astro/issues/5357#issuecomment-1314219452
+const shikiResourcePaths = Object.keys(
+	import.meta.glob([
+		"./node_modules/.pnpm/shiki@*/node_modules/shiki/languages/*.tmLanguage.json",
+		"./node_modules/.pnpm/shiki@*/node_modules/shiki/themes/*.json",
+	]),
+);
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: "server",
-  adapter: vercel()
+	adapter: vercel({
+		includeFiles: shikiResourcePaths,
+	}),
 });
