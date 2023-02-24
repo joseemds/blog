70438677bba8f1a44d6dc539dde709b37d96e00f
Jose Eduardo Monteiro <josee.edumont@gmail.com>
2023-02-24 00:34:47 -0300

    setup mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   output: "server",
   adapter: vercel()
 });
