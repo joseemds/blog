e64258331c5229f1b818f910fb4789abd7837a23
José Eduardo Monteiro <mds.josee@gmail.com>
2023-06-09 01:10:11 -0300

    make site more responsive

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,18 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		tags: z.string(),
-		draft: z.boolean(),
-		publishDate: z.string().transform(str => new Date(str)),
-		editDate: z.string().transform(str => new Date(str)).optional(),
-	})
+  schema: z.object({
+    title: z.string(),
+    tags: z.string(),
+    draft: z.boolean(),
+    publishDate: z.string().transform((str) => new Date(str)),
+    editDate: z
+      .string()
+      .transform((str) => new Date(str))
+      .optional(),
+  }),
 });
 
 export const collections = {
-	'blog': blogCollection,
+  blog: blogCollection,
 };
