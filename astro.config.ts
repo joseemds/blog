b61f7d9c269ae366ff1a14fe2a4714f162d4edaf
Jose Eduardo Monteiro <josee.edumont@gmail.com>
2023-12-20 00:29:40 -0300

    lot of tweaks

--- astro.config.ts
+++ astro.config.ts
@@ -1,32 +1,38 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
-import mochaTheme from "./src/assets/themes/latte.json"
+import mochaTheme from "./src/assets/themes/latte.json";
 import AutoImport from 'astro-auto-import';
 
 // https://github.com/withastro/astro/issues/5357#issuecomment-1314219452
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://letrec.in',
-	integrations: [tailwind(
-		{
-			config: { applyBaseStyles: false }
-		}
-	), AutoImport({
-		imports: [
-			{ './src/components/Title.astro': [['default', 'h2']] }
-		]
-	}), mdx()],
-	output: "static",
-	markdown: {
-		shikiConfig: { theme: mochaTheme }
-	},
+  site: 'https://letrec.in',
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), AutoImport({
+    imports: [{
+      './src/components/Title.astro': [['default', 'h2']]
+    }]
+  }), mdx(), react({
+			experimentalReactChildren: true
+		})],
+  output: "static",
+  markdown: {
+    shikiConfig: {
+      theme: mochaTheme
+    }
+  }
 });
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+	return (
+		<nav className="flex align-center justify-between px-8 text-xl pt-12 max-w-4xl mx-auto pb-4 font-source-sans">
+			<a href="/">Home</a>
+			<a href="/archive">Posts</a>
+		</nav>
+	)
+
+}
